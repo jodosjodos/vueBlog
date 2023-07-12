e4690482df9f62bc52cb3a3174802b5bde843dd4
jodosjodos <jeandedieu2030@gmail.com>
2023-07-12 20:03:16 +0200

    adding styles

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
         <p>Reaction time -{{ result }} ms</p>
+        <p class="rank">{{ rank }} </p>
     </div>
 </template> 
 <script>
 export default {
     name:"Results",
-    props:["result"]
+    props:["result"],
+    data(){
+        return {
+            rank:null
+        }
+    },mounted(){
+        if(this.result  < 250){
+            this.rank="rapiddly people"
+        }else if (this.rank < 400){
+            this.rank="meduim people"
+        }else {
+            this.rank="slowwly"
+        }
+    }
 }
 </script>
 <style>
+.rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
 </style>
